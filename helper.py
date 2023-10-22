c0c418c91bf93843ff0c0ed8217bf4ec4efb5d61
sysung <sysung98@berkeley.edu>
2023-10-22 17:07:19 +0000

    Flatten pandas dataframe

--- helper.py
+++ helper.py
@@ -5,19 +5,6 @@ import json
 
 from google.cloud import storage
 
-def explode_dictionary(pd_df: pd.DataFrame, field: str) -> pd.DataFrame:
-    '''
-    Explodes a dictionary within a column as multiple columns
-
-    Parameters:
-    pd_df (pd.Dataframe): The pandas dataframe to extract column dictionary
-    field (str): The column dictionary to explode
-
-    Returns:
-    pd.DataFrame: The new pandas dataframe with exploded column
-    '''
-    return pd.concat([pd_df.drop(field, axis=1), pd_df[field].apply(pd.Series)], axis=1)
-
 def download_json_into_memory(bucket_name, blob_name):
     """Downloads a json into memory."""
     # The ID of your GCS bucket
@@ -36,12 +23,6 @@ def download_json_into_memory(bucket_name, blob_name):
     # using `Bucket.blob` is preferred here.
     blob = bucket.blob(blob_name)
     contents = json.loads(blob.download_as_string())
-
-    print(
-        "Downloaded storage object {} from bucket {}.".format(
-            blob_name, bucket_name
-        )
-    )
 
     return contents
 
@@ -63,11 +44,6 @@ def get_pd_from_json(json_file: str) -> pd.DataFrame:
 
     # Transform JSON into Pandas dataframe
     pd_df = pd.DataFrame.from_dict(json_in_memory)
-
-    # Expand dictionaries into individual columns
-    # pd_df = explode_dictionary(pd_df, 'metadata')
-    # pd_df = explode_dictionary(pd_df, 'premise_articles')
-    # pd_df = explode_dictionary(pd_df, 'label')
 
     return pd_df
 
